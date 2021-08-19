ee5a397e063b91a82946017fd7062758745e2290
Victoria-FZ-eng <vzulof@gmail.com>
2021-08-19 23:08:45 +0300

    initializing app

---
+++ src/main/java/parkingHunter/example/parkingHunter/ParkingHunterApplication.java
@@ -0,0 +1,13 @@
+package parkingHunter.example.parkingHunter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingHunterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkingHunterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/parkingHunter/example/parkingHunter/ParkingHunterApplicationTests.java
@@ -0,0 +1,13 @@
+package parkingHunter.example.parkingHunter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingHunterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
