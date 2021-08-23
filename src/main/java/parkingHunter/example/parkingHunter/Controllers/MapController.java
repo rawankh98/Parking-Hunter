a56ac015aaf4a8ff31e4ff3a275157cf61df4036
Victoria-FZ-eng <vzulof@gmail.com>
2021-08-23 13:51:20 +0300

    add map

--- src/main/java/parkingHunter/example/parkingHunter/Controllers/MapController.java
+++ src/main/java/parkingHunter/example/parkingHunter/Controllers/MapController.java
@@ -4,8 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import parkingHunter.example.parkingHunter.Models.Parking;
 import parkingHunter.example.parkingHunter.Repos.MappingParkingRepositoriy;
+import parkingHunter.example.parkingHunter.Repos.ParkingRepository;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -14,6 +17,9 @@ public class MapController {
     MappingParkingRepositoriy mappingParkingRepositoriy;
         //    @Value("${tomtom.apikey}")
 //    private String tomTomApiKey;
+
+    @Autowired
+    ParkingRepository parkingRepository;
 
     @GetMapping("/map")
     public String homePage(Model model) {
@@ -26,14 +32,35 @@ public class MapController {
         System.out.println("MAP");
         System.out.println("*************************************************************************");
 
+        Iterable parkings= parkingRepository.findAll();
+        model.addAttribute("parkingsOwner",parkings);
+        model.addAttribute("coolLocations", coolLocations());
+
         return "tomtom";
     }
     private List<Location> coolLocations() {
-        return List.of(
 
-                new Location(new double[]{35.93448795290056, 31.94958622949373}, "Jamea' Al-Husseini")
+        List<Parking> parkings= (List<Parking>) parkingRepository.findAll();
+       List<Location> all = new ArrayList<>();
+       double lonLat[] = new double[2];
+       String name="";
 
-        );
+        for (int i = 0; i < parkings.size(); i++) {
+            lonLat[0]=Double.parseDouble(parkings.get(i).getLatitude());
+            lonLat[1]=Double.parseDouble(parkings.get(i).getLongitude());
+            name=parkings.get(i).getParkingName();
+
+           all.add(new Location( lonLat, name)) ;
+        }
+        all.add(new Location(new double[]{35.93448795290056, 31.94958622949373}, "Husseini"));
+        return all;
+//
+//        return List.of(
+//                new Location(new double[]{-121.901481, 36.618253}, "Monterey Bay Aquarium"),
+//                new Location(new double[]{21.006010, 52.231606}, "Palace of Culture and Science"),
+//                new Location(new double[]{2.337612, 48.860717}, "Louvre Museum")
+//        );
+
     }
 
     private static class Location {
